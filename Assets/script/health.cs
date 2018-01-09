b4e5c55cd1fa3ea331890f21d68108f472e122a0
zhg0003 <zhg0003@gmail.com>
2018-01-08 17:10:41 -0800

    added health and dmg taking

--- Assets/script/health.cs
+++ Assets/script/health.cs
@@ -8,19 +8,24 @@ public class health : MonoBehaviour {
     public int currentHealth;
     public int maxHealth;
     public Slider healthBar;
-
+    
 	// Use this for initialization
 	void Awake () {
         maxHealth = 100;
         currentHealth = startingHealth;
         print("health script start");
-	}
+        //healthBar = GetComponent<Slider>();
+        healthBar.value = currentHealth / maxHealth;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown("k"))
         {
             currentHealth -= 1;
+            healthBar.value = (float)currentHealth / maxHealth;
+            print("health bar value " + healthBar.value);
+            print("current health value " + currentHealth);
         }
         if (currentHealth == 0)
             print("dead");
